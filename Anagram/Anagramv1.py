72f5337708d938ce17d548cf19984cd8286bb6c4
Mathieu-93 <ogonowskimateusz@yahoo.pl>
2019-12-26 15:28:26 +0100

    Anagram

---
+++ Anagram/Anagramv1.py
@@ -0,0 +1,35 @@
+def anagram (s1,s2):
+    #Creat a chain of sign without spaces and capital letters
+    s1=s1.replace(' ','').lower() 
+    s2=s2.replace(' ','').lower()
+    
+    #It's crucial to have the same lenght
+    if len(s1) != len(s2):
+        return False
+    
+    #Empty dictionary
+    count={}
+    
+    #Asign to every charater number 
+    for i in s1:
+        if i in count:
+            count[i]+=1
+        else:
+            count[i]=1
+    #Substract till achive empty dictionary
+    for j in s2:
+        if j in count:
+            count[j]-=1
+        else:
+            count[j]=1
+    #return False when dictionary have any charaters
+    for k in count:
+        if count[k] != 0:
+            return False
+    return True
+
+#Simply test
+print("Test alghoritm is dog equal to god?:")
+print(anagram('dog','god'))
+print("Clint Eastwood play in Old west action?")
+print(anagram('clint eastwood','old west action'))
